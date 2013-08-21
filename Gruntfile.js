555a0b3cb207eed6940cedee76bba5ab728d0817
Ionut Colceriu <contact@ghinda.net>
2013-08-21 18:21:27 +0300

    More work on moving to Sass with Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,93 +19,93 @@ module.exports = function (grunt) {
 		yeomanConfig.app = require('./bower.json').appPath || yeomanConfig.app;
 	} catch (e) {}
 
-  grunt.initConfig({
-    yeoman: yeomanConfig,
-    watch: {
-      compass: {
-        files: [ '<%= yeoman.app %>/{,*/}*.{scss,sass}' ],
-        tasks: [ 'compass:server' ]
-      },
-      livereload: {
-        options: {
-          livereload: LIVERELOAD_PORT
-        },
-        files: [
-          './{,*/}*.html',
-          './{,*/}*.css'
-        ]
-      }
-    },
-    connect: {
-      options: {
-        port: 9000,
-        // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost'
-      },
-      livereload: {
-        options: {
-          middleware: function (connect) {
-            return [
-              lrSnippet,
-              mountFolder(connect, './')
-            ];
-          }
-        }
-      },
-      dist: {
-        options: {
-          middleware: function (connect) {
-            return [
-              mountFolder(connect, yeomanConfig.dist)
-            ];
-          }
-        }
-      }
-    },
-    open: {
-      server: {
-        url: 'http://localhost:<%= connect.options.port %>'
-      }
-    },
-    compass: {
-      options: {
-        sassDir: '<%= yeoman.app %>',
-        cssDir: './dist'
-      },
-      dist: {},
-      server: {
-        options: {
-          debugInfo: true
-        }
-      }
-    },
-    concurrent: {
-      server: [
-        'compass:server'
-      ],
-      dist: [
-        'compass:dist'
-      ]
-    }
-  });
+	grunt.initConfig({
+		yeoman: yeomanConfig,
+		watch: {
+		compass: {
+			files: [ '<%= yeoman.app %>/{,*/}*.{scss,sass}' ],
+			tasks: [ 'compass:server' ]
+		},
+		livereload: {
+			options: {
+				livereload: LIVERELOAD_PORT
+			},
+			files: [
+				'./{,*/}*.html',
+				'./{,*/}*.css'
+			]
+		}
+		},
+		connect: {
+		options: {
+			port: 9000,
+			// Change this to '0.0.0.0' to access the server from outside.
+			hostname: 'localhost'
+		},
+		livereload: {
+			options: {
+			middleware: function (connect) {
+				return [
+				lrSnippet,
+				mountFolder(connect, './')
+				];
+			}
+			}
+		},
+		dist: {
+			options: {
+				middleware: function (connect) {
+					return [
+					mountFolder(connect, yeomanConfig.dist)
+					];
+				}
+			}
+		}
+		},
+		open: {
+			server: {
+				url: 'http://localhost:<%= connect.options.port %>'
+			}
+		},
+		compass: {
+			options: {
+				sassDir: '<%= yeoman.app %>',
+				cssDir: './dist'
+			},
+			dist: {},
+			server: {
+				options: {
+					debugInfo: false
+				}
+			}
+		},
+		concurrent: {
+			server: [
+				'compass:server'
+			],
+			dist: [
+				'compass:dist'
+			]
+		}
+	});
 
-  grunt.registerTask('server', function (target) {
-    if (target === 'dist') {
-      return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
-    }
+	grunt.registerTask('server', function (target) {
+		if (target === 'dist') {
+			return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+		}
 
-    grunt.task.run([
-      'concurrent:server',
-      'connect:livereload',
-      'watch'
-    ]);
-  });
+		grunt.task.run([
+			'concurrent:server',
+			'connect:livereload',
+			'watch'
+		]);
+	});
 
-  grunt.registerTask('build', [
-    'concurrent:dist'
-  ]);
+	grunt.registerTask('build', [
+		'concurrent:dist'
+	]);
 
-  grunt.registerTask('default', [
-    'build'
-  ]);
+	grunt.registerTask('default', [
+		'build'
+	]);
 };
