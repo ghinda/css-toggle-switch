6649760853388578be0947341f00629fe7138b52
Henjo Hoeksma <henjo@simplyadmire.com>
2014-09-26 12:06:39 +0200

    Multiple output types
    Generates px version from base
    Use base version to generate rem and em version
    
    Only px version needs to be maintained.

--- Gruntfile.js
+++ Gruntfile.js
@@ -65,6 +65,8 @@ module.exports = function (grunt) {
       dist: {
         files: {
           '<%= yeoman.dist %>/toggle-switch.css': '<%= yeoman.app %>/toggle-switch.scss',
+          '<%= yeoman.dist %>/toggle-switch-rem.css': '<%= yeoman.app %>/toggle-switch-rem.scss',
+          '<%= yeoman.dist %>/toggle-switch-em.css': '<%= yeoman.app %>/toggle-switch-em.scss',
           '<%= yeoman.dist %>/docs/docs.css': '<%= yeoman.app %>/docs/docs.scss',
           '<%= yeoman.dist %>/docs/foundation.css': 'bower_components/foundation/scss/foundation.scss'
         }
@@ -77,6 +79,8 @@ module.exports = function (grunt) {
         },
         files: {
           '<%= yeoman.dist %>/toggle-switch.css': '<%= yeoman.app %>/toggle-switch.scss',
+          '<%= yeoman.dist %>/toggle-switch-rem.css': '<%= yeoman.app %>/toggle-switch-rem.scss',
+          '<%= yeoman.dist %>/toggle-switch-em.css': '<%= yeoman.app %>/toggle-switch-em.scss',
           '<%= yeoman.dist %>/docs/docs.css': '<%= yeoman.app %>/docs/docs.scss',
           '<%= yeoman.dist %>/docs/foundation.css': 'bower_components/foundation/scss/foundation.scss'
         }
